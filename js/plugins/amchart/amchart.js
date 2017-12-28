79412e761c433139620ce1394170d345bba3cb4b
{arsapoz} <{arsaaa@live.com}>
2017-12-28 15:18:30 +0100

    mapa bez pinova

--- js/plugins/amchart/amchart.js
+++ js/plugins/amchart/amchart.js
@@ -1,20 +1,66 @@
-var map = AmCharts.makeChart( "chartdiv", {
-
-  "type": "map",
-  "theme": "light",
-  "projection": "miller",
-
-  "dataProvider": {
-    "map": "worldLow",
-    "getAreasFromMap": true
+var map = AmCharts.makeChart("mapdiv",{
+type: "map",
+theme: "dark",
+projection: "miller",
+panEventsEnabled : true,
+backgroundColor : "#3e4149",
+backgroundAlpha : 1,
+zoomControl: {
+zoomControlEnabled : true
+},
+dataProvider : {
+map : "worldHigh",
+getAreasFromMap : true,
+zoomLevel: 4.5,
+zoomLongitude: 20,
+zoomLatitude: 45,
+areas :
+[
+  {
+    "id": "AT",
+    "showAsSelected": true
   },
-  "areasSettings": {
-    "autoZoom": true,
-    "selectedColor": "#CC0000"
+  {
+    "id": "CZ",
+    "showAsSelected": true
   },
-  "smallMap": {},
-  "export": {
-    "enabled": true,
-    "position": "bottom-right"
+  {
+    "id": "FR",
+    "showAsSelected": true
+  },
+  {
+    "id": "DE",
+    "showAsSelected": true
+  },
+  {
+    "id": "GR",
+    "showAsSelected": true
+  },
+  {
+    "id": "IT",
+    "showAsSelected": true
+  },
+  {
+    "id": "RO",
+    "showAsSelected": true
+  },
+  {
+    "id": "RS",
+    "showAsSelected": true
+  },
+  {
+    "id": "TR",
+    "showAsSelected": true
   }
-} );
+]
+},
+areasSettings : {
+autoZoom : true,
+color : "#444f59",
+colorSolid : "#fec8c9",
+selectedColor : "#fec8c9",
+outlineColor : "#FEFAEC",
+rollOverColor : "#fd9  99a",
+rollOverOutlineColor : "#3e4149",
+}
+});
